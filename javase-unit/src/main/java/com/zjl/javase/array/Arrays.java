f79c2856178068c72fa94fa4219812b687a05c5d
zhangjiale <1102092903@qq.com>
2018-08-17 17:29:58 +0800

    tijiao

---
+++ javase-unit/src/main/java/com/zjl/javase/array/Arrays.java
@@ -0,0 +1,11 @@
+package com.zjl.javase.array;
+
+public class Arrays {
+	public static void main(String[] args) {
+		//声明数组
+		String [] arry;
+		String arry2 [];
+		String [] arry3 = new String[5];
+		int score [] = new int[3];
+	}
+}
