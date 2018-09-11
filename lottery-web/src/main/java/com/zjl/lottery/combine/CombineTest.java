45bbb76473a058691f65c93427c7c7a011dfc93a
zhangjiale <514615915@qq.com>
2018-09-11 22:05:40 +0800

    hjh

--- lottery-web/src/main/java/com/zjl/lottery/combine/CombineTest.java
+++ lottery-web/src/main/java/com/zjl/lottery/combine/CombineTest.java
@@ -6,8 +6,8 @@ public class CombineTest {
 	private static ArrayList<Integer> tmpArr = new ArrayList<Integer>();
 	public static void main(String[] args) {
 		//int [] param = {1,3,7,8,11,13,21,28,31,32,33};
-		int [] param = {1,3,8,19,20,23};
-		int nums = 2;
+		int [] param = {30,15,4,12,32,1,9,28,29,31,6,18,22,33};
+		int nums = 6;
 		System.out.println("组合结果：");
         combine(0 ,nums ,param);
 	}
@@ -24,13 +24,21 @@ public class CombineTest {
             for (int i = index; i < param.length; i++) {
                 tmpArr.add(param[i]);
                 int count = 0;
+                boolean falg1 = false;
+                boolean falg2 = false;
                 for (int j = 0; j < tmpArr.size(); j++) {
 					int temp = tmpArr.get(j);
 					if(temp%2 == 0){
 						count ++;
 					}
+					if(!falg1 && temp==30){
+						falg1 = true;
+					}
+					if(!falg2 && temp==15){
+						falg2 = true;
+					}
 				}
-                if(count != 0){
+                if(count > 1 && falg1 || falg2){
                     System.out.println(tmpArr.toString() + ",");
                 }
                 tmpArr.remove((Object)param[i]);
