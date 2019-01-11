76443796c837110553b591dd7d29f40f783839e8
zhangjiale <1102092903@qq.com>
2019-01-11 14:36:05 +0800

    leecode 第10题

---
+++ leecode-unit/src/main/java/com/zjl/leecode/test/Test10.java
@@ -0,0 +1,12 @@
+package com.zjl.leecode.test;
+
+import com.zjl.leecode.solutions.Solution10;
+
+public class Test10 {
+	public static void main(String[] args) {
+		String param = "aa";
+		String reg = "aa";
+		boolean result = Solution10.isMatch(param,reg);
+		System.out.println(result);
+	}
+}
