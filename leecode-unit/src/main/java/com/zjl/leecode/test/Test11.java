91db7d605a4be1309b5a6853dbdec88a85920931
zhangjiale <1102092903@qq.com>
2019-01-11 14:52:10 +0800

    leecode 第11题提交

---
+++ leecode-unit/src/main/java/com/zjl/leecode/test/Test11.java
@@ -0,0 +1,11 @@
+package com.zjl.leecode.test;
+
+import com.zjl.leecode.solutions.Solution11;
+
+public class Test11 {
+	public static void main(String[] args) {
+		int [] param = {1,8,6,2,5,4,8,3,7};
+		int result = Solution11.maxArea(param);
+		System.out.println(result);
+	}
+}
