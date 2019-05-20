b55e78281927a014bdd536a8dff366e18ef82aa8
zhangjiale <1102092903@qq.com>
2019-05-20 18:27:41 +0800

    888

--- lottery-web/src/main/java/com/zjl/lottery/combine/CombineMain.java
+++ lottery-web/src/main/java/com/zjl/lottery/combine/CombineMain.java
@@ -21,7 +21,7 @@ public class CombineMain {
 			LotteryTypEnum lotteryTyp = LotteryTypEnum.GREAT_LOTTO;
 			ScreenTypEnum screenTypEnum = ScreenTypEnum.RED;
 			Integer screenNum = 5;
-			int[] otherArr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,30,31,32,33,34,35};
+			int[] otherArr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35};
 			//期数
 			String periods = "057";
 			CombineGenerate combineGenerate = InitCombineParam.init(lotteryTyp,screenTypEnum,screenNum,otherArr,periods);
