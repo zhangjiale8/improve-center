e2b70e632263e828dc703426c8b4d7061a5213e9
zhangjiale <514615915@qq.com>
2019-05-20 21:44:28 +0800

    888

--- lottery-web/src/main/java/com/zjl/lottery/combine/util/CombineSelect.java
+++ lottery-web/src/main/java/com/zjl/lottery/combine/util/CombineSelect.java
@@ -7,9 +7,9 @@ public class CombineSelect {
 	private static ArrayList<Integer> tmpArr = new ArrayList<Integer>();
 	private static ArrayList<String> loterryLsit = new ArrayList<String>();
 	public static void main(String[] args) {
-		int [] param = {1,2,4,5,7,8,9,12,13,14,15,16,17,18,19,21,22,23,24,25,26,28,29,30,31,33};
+		int [] param = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35};
 	
-		int nums = 6;
+		int nums = 5;
 		System.out.println("组合结果：");
         combine(0 ,nums ,param);
         int totalSize = loterryLsit.size();
