50dc744cef432afacf8022fac24fc4c84aadee09
zhangjiale <514615915@qq.com>
2019-05-26 13:13:55 +0800

    888

--- lottery-web/src/main/java/com/zjl/lottery/db/DbOperateMain.java
+++ lottery-web/src/main/java/com/zjl/lottery/db/DbOperateMain.java
@@ -1,39 +1,78 @@
 package com.zjl.lottery.db;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang.StringUtils;
 
 import com.zjl.lottery.db.util.JDBCPatchUtil;
 import com.zjl.lottery.db.util.JDBCUtil;
+import com.zjl.lottery.mutitest.LotteryHaveNoMaster;
+import com.zjl.tools.ArrayTool;
 import com.zjl.tools.TimeTools;
 
 public class DbOperateMain {
 	public static void main(String[] args) {
-		Connection conn = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        try {
-           
-            DoubleBallDTO doubleballdto = new DoubleBallDTO("2019059", new Date(), new Date(), "2019", "059", "1,2,3,5,8", "5");
-            //doubleballdto.insertDrawRecord();
-            ArrayList<DoubleBallDTO> list = new ArrayList<DoubleBallDTO>();
-            list.add(doubleballdto);
-            list.add(doubleballdto);
-            list.add(doubleballdto);
-            list.add(doubleballdto);
-            list.add(doubleballdto);
-            list.add(doubleballdto);
-            JDBCPatchUtil.insertDrawBatch(list);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally{
-            JDBCUtil.close(rs, ps, conn);
-        }
+		ArrayList<DoubleBallDTO> list = new ArrayList<DoubleBallDTO>();
+		String ticketPath = "data/doubleball.txt";
+		URL url = LotteryHaveNoMaster.class.getClassLoader().getResource(ticketPath);
+		File file = new File(url.getFile());
+		if(null != file && file.exists()) {
+			//构造一个BufferedReader类来读取文件
+			try {
+				int index = 0;
+				BufferedReader br = new BufferedReader(new FileReader(file));
+				String line = null;
+				DoubleBallDTO doubleballdto = null;
+				while((StringUtils.isNotEmpty(line = br.readLine()))){//使用readLine方法，一次读一行
+					index++;
+					if(index % 2 == 0){
+						if(null != doubleballdto){
+							String temp = line.replaceAll(" ", ",");
+			                String [] codeArr = temp.split(",");
+			                String redcodeVal = "";
+			                String[] redArr = Arrays.copyOfRange(codeArr, 0, 6);
+			                for (int i = 0; i < redArr.length; i++) {
+			                	redcodeVal += (redArr[i] +",").replaceAll(" ","");
+							}
+			                redcodeVal = redcodeVal.substring(0, redcodeVal.length()-1);
+							String bluecodeVal = codeArr[6];
+							doubleballdto.setRedcodeVal(redcodeVal);
+							doubleballdto.setBluecodeVal(bluecodeVal);
+							list.add(doubleballdto);
+						}
+					}else{
+						String [] issueArr = line.split("@");
+						String drawId = issueArr[0];
+						Date drawDtm = TimeTools.DateFormate(issueArr[1], TimeTools.Y_M_D_H_M_S);
+						Date createDtm = new Date();
+						String year = drawId.substring(0, 4);
+						String issueNum = drawId.substring(4, drawId.length());
+						doubleballdto = new DoubleBallDTO(drawId,drawDtm,createDtm,year,issueNum,null,null);
+					}
+	               
+	            }
+				if(null != list && list.size() > 0){
+					 JDBCPatchUtil.insertDrawBatch(list);
+				}
+	            br.close(); 
+	            
+			} catch (Exception e) {
+				e.getStackTrace();
+			}
+		}
+	
     }
 	
 }
