a762dac632bc1548b4358bc4840b09fee17919d4
zhangjiale <1102092903@qq.com>
2019-06-03 19:23:25 +0800

    88888

--- book-unit/src/main/java/com/zjl/interview/year2019/designmodel/javase/Collections/method/Test9.java
+++ book-unit/src/main/java/com/zjl/interview/year2019/designmodel/javase/Collections/method/Test9.java
@@ -7,19 +7,19 @@ import java.util.List;
 
 /**
  * 
- * @Description: lastIndexOfSubList(List source,List target)方法的使用与上例方法的使用相同
+ * @Description: indexOfSubList(List list,List subList)方法的使用(含义：查找subList在list中首次出现位置的索引)。
 
  * @author: zhangjiale
  * @date:   2019年6月3日 上午10:48:01   
  *     
  * @Copyright: 2019
  */
-public class Test8 {
+public class Test9 {
 	public static void main(String[] args) {
 		List list = Arrays.asList("7,2,0,9,5,0,9,5".split(","));
 		List list2 = Arrays.asList("0,9,5".split(","));
 		System.out.println(list);
-		System.out.println(Collections.lastIndexOfSubList(list, list2));
+		System.out.println(Collections.indexOfSubList(list, list2));
 	
 	}
 }
