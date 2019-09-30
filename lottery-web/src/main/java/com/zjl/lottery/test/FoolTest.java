ab4e94f85b3bef7a31d53e8b088a998639a46072
zhangjiale <1102092903@qq.com>
2019-09-30 13:54:43 +0800

    88888

---
+++ lottery-web/src/main/java/com/zjl/lottery/test/FoolTest.java
@@ -0,0 +1,97 @@
+package com.zjl.lottery.test;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import com.zjl.lottery.util.ListTxtUtil;
+import com.zjl.lottery.util.MapTxtUtil;
+import com.zjl.tools.ArrayTool;
+
+public class FoolTest {
+	public static void main(String[] args) {
+		//singlescreen();
+		multipscreen();
+	}
+	
+	/**
+	 * 单组过滤
+	 * @param:       
+	 */
+	private static void singlescreen() {
+
+		String [] filenameArr = {
+				"resultmapdeterminedcludehistorycludeotherticketclude",
+				"resultmapdeterminedcludehistorycludeotherticketunclude",
+				"resultmapdeterminedcludehistoryuncludeotherticketclude",
+				"resultmapdeterminedcludehistoryuncludeotherticketunclude",
+				"resultmapdetermineduncludehistorycludeotherticketclude",
+				"resultmapdetermineduncludehistorycludeotherticketunclude",
+				"resultmapdetermineduncludehistoryuncludeotherticketclude",
+				"resultmapdetermineduncludehistoryuncludeotherticketunclude"
+				};
+		int[] screenIntArr = {3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,22,23,25,26,27,29,30,31};
+		String[] screenArr = ArrayTool.intArr2StrArr(screenIntArr);
+		for (String filename : filenameArr) {
+			filename += "threescreen";
+			String filepath = "E:" + File.separator + "screen" + File.separator +filename+".txt";;
+			Map<String, Integer> datamap = MapTxtUtil.getDataMap(filepath);
+			Map<String, Integer> resultmap = new HashMap<String, Integer>();
+			for (Entry<String, Integer> entry : datamap.entrySet()) {
+				String[] combineArr = entry.getKey().split(",");
+				String[] intersectArr = ArrayTool.getIntersectArr(screenArr, combineArr);
+				if(intersectArr.length > 3) {
+					resultmap.put(entry.getKey(), entry.getValue());
+				}
+				
+			}
+			MapTxtUtil.createScreenTxtMap(resultmap, filename+"0001");
+		}
+		
+	}
+	/**
+	 * 多组过滤
+	 * @param:       
+	 */
+	private static void multipscreen() {
+
+
+		String [] filenameArr = {
+				"resultmapdeterminedcludehistorycludeotherticketclude",
+				"resultmapdeterminedcludehistorycludeotherticketunclude",
+				"resultmapdeterminedcludehistoryuncludeotherticketclude",
+				"resultmapdeterminedcludehistoryuncludeotherticketunclude",
+				"resultmapdetermineduncludehistorycludeotherticketclude",
+				"resultmapdetermineduncludehistorycludeotherticketunclude",
+				"resultmapdetermineduncludehistoryuncludeotherticketclude",
+				"resultmapdetermineduncludehistoryuncludeotherticketunclude"
+				};
+		String screenfilepath = "E:" + File.separator + "screen" + File.separator +"sceenlist.txt";;
+		List<String[]> screenarrlist = ListTxtUtil.getScreenList(screenfilepath);
+		for (int i = 0; i < screenarrlist.size(); i++) {
+			String[] screenArr = screenarrlist.get(i);
+			for (String filename : filenameArr) {
+				filename += "threescreen";
+				if(i > 0) {
+					filename += "threescreen"+i;
+				}
+				String filepath = "E:" + File.separator + "screen" + File.separator +filename+".txt";;
+				Map<String, Integer> datamap = MapTxtUtil.getDataMap(filepath);
+				Map<String, Integer> resultmap = new HashMap<String, Integer>();
+				for (Entry<String, Integer> entry : datamap.entrySet()) {
+					String[] combineArr = entry.getKey().split(",");
+					String[] intersectArr = ArrayTool.getIntersectArr(screenArr, combineArr);
+					if(intersectArr.length > 1) {
+						resultmap.put(entry.getKey(), entry.getValue());
+					}
+					
+				}
+				MapTxtUtil.createScreenTxtMap(resultmap, filename+(i+1));
+			}			
+			
+		}
+		
+	}
+}
--- lottery-web/src/main/java/com/zjl/lottery/util/ListTxtUtil.java
+++ lottery-web/src/main/java/com/zjl/lottery/util/ListTxtUtil.java
@@ -1,9 +1,14 @@
 package com.zjl.lottery.util;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.Writer;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
 
@@ -33,5 +38,35 @@ public class ListTxtUtil {
 			}
 		}
 		
+	}
+	
+	/**
+	 * 返回过滤数组集合
+	 * @param filePath
+	 * @return
+	 */
+	public static List<String[]> getScreenList(String filePath) {
+		List<String[]> list = new ArrayList<String[]>();
+		if(StringUtils.isNotEmpty(filePath)){
+			try {
+				File file = new File(filePath);
+				if(null != file && file.exists()) {
+
+					BufferedReader br = new BufferedReader(new FileReader(file));
+					String line = null;
+					while((StringUtils.isNotEmpty(line = br.readLine()))){//使用readLine方法，一次读一行
+						String[] lineArr = line.split(",");
+						if(null != lineArr && lineArr.length > 0) {
+							list.add(lineArr);
+						}
+		            }
+		            br.close();
+				
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return list;
 	}
 }
