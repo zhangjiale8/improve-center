38f7fb675eb0e02e281c2fe62dcb12966ad8c799
zhangjiale <1102092903@qq.com>
2019-10-15 13:03:33 +0800

    88888

--- lottery-web/src/main/java/com/zjl/lottery/test/MultipParamsListTest.java
+++ lottery-web/src/main/java/com/zjl/lottery/test/MultipParamsListTest.java
@@ -17,18 +17,18 @@ import com.zjl.lottery.util.TwoSreenUtil;
 public class MultipParamsListTest {
 	public static void main(String[] args) {
 
-		int[] paramArr1 = {1,2,3,4,6,7,8,9,10,11,12,13,14,15,16,17,19,21,22,24,26,27,28,30,33};
-		int[] paramArr2 = {1,2,5,6,7,8,9,10,11,12,15,16,17,19,20,21,22,23,24,26,27,28,30,32,33};
-		int[] paramArr3 = {2,3,4,5,6,8,9,10,12,13,15,16,18,19,20,21,22,24,25,27,28,29,30,32,33};
-		int[] paramArr4 = {2,4,6,8,9,10,11,12,14,15,16,17,18,19,20,21,22,23,24,26,27,28,30,31,32};
-		int[] paramArr5 = {1,2,3,4,5,6,7,9,10,11,13,14,15,17,18,19,20,22,23,26,28,29,30,31,32};
-		int[] paramArr6 = {1,2,3,5,7,8,10,11,13,14,15,16,17,18,19,20,21,25,26,27,28,29,30,32,33};
-		int[] paramArr7 = {1,2,3,4,5,6,7,9,11,12,14,17,18,19,20,21,22,23,24,25,26,27,28,30,31};
-		int[] paramArr8 = {1,2,4,5,6,7,8,9,11,13,14,15,16,17,18,20,21,22,23,24,26,27,28,29,30};
-		int[] paramArr9 = {1,2,3,5,6,9,10,11,12,13,14,15,16,17,18,19,20,21,22,25,26,28,29,31,32};
-		int[] paramArr10 = {1,2,5,6,7,8,9,10,11,12,13,16,17,18,19,22,23,25,26,27,28,29,31,32,33};
-		int[] paramArr11 = {1,2,3,4,5,6,8,11,12,13,15,17,18,19,20,21,24,25,27,28,29,30,31,32,33};
-		int[] paramArr12 = {1,3,4,5,7,9,10,12,13,15,16,17,18,20,21,23,24,26,27,28,29,30,31,32,33};
+		int[] paramArr1 = {};
+		int[] paramArr2 = {};
+		int[] paramArr3 = {};
+		int[] paramArr4 = {};
+		int[] paramArr5 = {};
+		int[] paramArr6 = {};
+		int[] paramArr7 = {};
+		int[] paramArr8 = {};
+		int[] paramArr9 = {};
+		int[] paramArr10 = {};
+		int[] paramArr11 = {};
+		int[] paramArr12 = {};
 		ArrayList<int[]> screenlist = new ArrayList<int[]>();
 		screenlist.add(paramArr12);
 		screenlist.add(paramArr11);
@@ -42,7 +42,7 @@ public class MultipParamsListTest {
 		screenlist.add(paramArr3);
 		screenlist.add(paramArr2);
 		screenlist.add(paramArr1);
-		int[] screenarray = {1,2,3,4,5,6,7,8,9,11,12,13,14,15,17,18,19,20,21,22,23,24,25,26,27,28,30,31,32,33};
+		int[] screenarray = {};
 	
 		//MultipParamsListUtil.screenDetermined(screenlist, screenarray);
 		//historyscreen();
@@ -53,7 +53,43 @@ public class MultipParamsListTest {
 		
 	}
 	
+	public void init(){
 
+		int[] paramArr1 = {};
+		int[] paramArr2 = {};
+		int[] paramArr3 = {};
+		int[] paramArr4 = {};
+		int[] paramArr5 = {};
+		int[] paramArr6 = {};
+		int[] paramArr7 = {};
+		int[] paramArr8 = {};
+		int[] paramArr9 = {};
+		int[] paramArr10 = {};
+		int[] paramArr11 = {};
+		int[] paramArr12 = {};
+		ArrayList<int[]> screenlist = new ArrayList<int[]>();
+		screenlist.add(paramArr12);
+		screenlist.add(paramArr11);
+		screenlist.add(paramArr10);
+		screenlist.add(paramArr9);
+		screenlist.add(paramArr8);
+		screenlist.add(paramArr7);
+		screenlist.add(paramArr6);
+		screenlist.add(paramArr5);
+		screenlist.add(paramArr4);
+		screenlist.add(paramArr3);
+		screenlist.add(paramArr2);
+		screenlist.add(paramArr1);
+		int[] screenarray = {};
+	
+		//MultipParamsListUtil.screenDetermined(screenlist, screenarray);
+		//historyscreen();
+		//otherticketscreen();
+		//threeScreen();
+		//twoScreen();
+		pingtotal();
+		
+	}
 	/**
 	 * 历史开奖信息过滤
 	 */
