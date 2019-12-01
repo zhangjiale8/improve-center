b47488a0f5fe3a67c7ef149847645be93f50f21c
zhangjiale <1102092903@qq.com>
2019-12-01 15:22:15 +0800

    88888

--- lottery-web/src/main/java/com/zjl/lottery/doubleball/test/MultipParamsListTest.java
+++ lottery-web/src/main/java/com/zjl/lottery/doubleball/test/MultipParamsListTest.java
@@ -21,18 +21,18 @@ import com.zjl.lottery.util.TwoSreenUtil;
 public class MultipParamsListTest {
 	public static void main(String[] args) {
 
-		int[] paramArr1 = {1,2,3,4,6,7,9,11,12,13,14,15,18,20,21,22,23,25,26,27,28,29,30,31,33};
-		int[] paramArr2 = {1,2,3,4,5,6,7,8,12,13,14,15,16,17,19,20,21,22,23,24,25,27,29,30,31};
-		int[] paramArr3 = {1,3,4,5,6,7,8,10,11,12,13,14,15,16,17,18,19,20,23,25,27,28,30,31,32};
-		int[] paramArr4 = {1,2,3,4,5,6,7,8,9,10,13,14,16,17,19,20,21,22,26,27,28,30,31,32,33};
-		int[] paramArr5 = {1,2,3,4,5,8,10,11,12,13,14,16,17,19,20,22,23,24,25,26,27,28,29,31,32};
-		int[] paramArr6 = {1,2,3,4,6,7,8,9,11,12,14,15,16,17,19,20,21,22,24,26,29,30,31,32,33};
-		int[] paramArr7 = {3,4,6,7,10,12,13,15,16,17,18,19,20,21,22,23,25,26,27,28,29,30,31,32,33};
-		int[] paramArr8 = {1,2,4,6,8,9,10,11,12,13,14,15,16,17,19,21,22,23,25,27,28,30,31,32,33};
-		int[] paramArr9 = {1,3,4,5,6,8,9,10,11,13,14,16,17,19,20,21,22,23,25,27,28,29,30,31,32};
-		int[] paramArr10 = {1,2,3,4,6,8,9,10,11,13,14,15,16,17,18,19,20,22,24,25,27,28,29,32,33};
-		int[] paramArr11 = {1,2,3,4,5,7,9,12,15,16,17,18,19,21,22,23,24,26,27,28,29,30,31,32,33};
-		int[] paramArr12 = {1,2,3,4,7,8,9,10,13,15,16,18,19,20,21,22,24,25,26,27,28,29,30,31,33};
+		int[] paramArr1 = {1,2,3,5,7,8,9,10,11,12,13,14,15,16,17,19,20,24,25,26,27,30,31,32,33};
+		int[] paramArr2 = {2,3,4,5,8,9,11,12,13,14,15,16,17,18,19,20,21,23,25,26,27,28,29,30,33};
+		int[] paramArr3 = {1,3,4,5,6,7,8,9,11,12,13,15,16,17,18,20,21,22,23,24,25,28,29,30,33};
+		int[] paramArr4 = {1,4,5,7,8,10,11,12,14,15,16,17,18,19,21,22,23,24,26,27,28,29,30,32,33};
+		int[] paramArr5 = {1,2,3,4,5,7,8,9,10,11,12,13,15,16,17,18,19,21,22,23,24,25,26,28,33};
+		int[] paramArr6 = {1,3,4,6,7,8,9,10,11,12,14,15,16,17,19,20,21,22,24,27,29,30,31,32,33};
+		int[] paramArr7 = {1,2,4,5,6,8,10,11,12,13,15,16,18,19,20,21,22,23,25,26,27,28,31,32,33};
+		int[] paramArr8 = {1,2,4,5,6,8,9,11,12,13,14,17,19,20,21,23,24,25,26,27,28,29,30,31,33};
+		int[] paramArr9 = {1,2,4,5,7,8,9,10,11,13,16,17,18,19,20,22,24,25,26,27,28,29,30,31,33};
+		int[] paramArr10 = {1,3,7,9,10,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,29,30,31,32};
+		int[] paramArr11 = {1,2,3,5,6,9,10,12,13,14,15,16,17,18,20,21,22,23,24,25,26,28,29,31,33};
+		int[] paramArr12 = {1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,21,22,24,25,27,28,29,30,31,33};
 		ArrayList<int[]> screenlist = new ArrayList<int[]>();
 		screenlist.add(paramArr12);
 		screenlist.add(paramArr11);
@@ -54,9 +54,9 @@ public class MultipParamsListTest {
 	//	otherticketscreen();
 	//	threeScreen();
 		//twoScreen();
-	//	pingtotal();
+		pingtotal();
 	//	sametails();
-		randomaward(); 
+	//	randomaward(); 
 		
 	}
 	/**
@@ -75,7 +75,7 @@ public class MultipParamsListTest {
 	}
 	//随机选取
 	private static void randomaward() {
-		String filepath = "E:" + File.separator + "screen" + File.separator +"resultmap.txt";;
+		String filepath = "E:" + File.separator + "screen" + File.separator +"pingtotalsametails.txt";;
 		Map<String, Integer> datamap = MapDataUtil.getDataMap(filepath);
 		List<String> initlist = new ArrayList<String>();
 		for (Entry<String, Integer> entry : datamap.entrySet()) {
@@ -83,7 +83,7 @@ public class MultipParamsListTest {
 		}
 		Map<String, Integer> firstdraw = new HashMap<String, Integer>();
 		String data = "";
-		for (int i = 0; i < 2; i++) {
+		for (int i = 0; i < 7; i++) {
 			double random = (double) ((Math.random()*9+1)*1000)/10000;
 			int index = Integer.parseInt(new java.text.DecimalFormat("0").format(random*initlist.size()));
 			String draw = initlist.get(index);
@@ -260,7 +260,7 @@ public class MultipParamsListTest {
 				};
 		Map<String, Integer> totalmap = new HashMap<String, Integer>();
 		for (String filename : filenameArr) {
-			filename += "threescreen1234567";
+			filename += "threescreen12345";
 			String filepath = "E:" + File.separator + "screen" + File.separator +filename+".txt";;
 			Map<String, Integer> datamap = MapDataUtil.getDataMap(filepath);
 			for (Entry<String, Integer> entry : datamap.entrySet()) {
