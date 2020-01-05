24207a65441fac391f0ccf14096ddaece80e2ef1
zhangjiale <514615915@qq.com>
2020-01-05 23:21:35 +0800

    8888

--- lottery-web/src/main/java/com/zjl/lottery/doubleball/test/tail/Test1.java
+++ lottery-web/src/main/java/com/zjl/lottery/doubleball/test/tail/Test1.java
@@ -5,20 +5,22 @@ import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
 
+import com.zjl.lottery.doubleball.util.HistoryUtil;
 import com.zjl.lottery.doubleball.util.TailUtil;
 
 public class Test1 {
 	public static void main(String[] args) {
 		Map<String, String> periodtailsmap = TailUtil.getPeriodTailsMap();
+		Map<String, String> historymap =  HistoryUtil.getPeriodHistoryMap();
 		//Map<String, Integer> tailsmap = TailUtil.getTailsStatisticsMap();
 		/*String[] comptails = {"23","1","2"};
 		String [] tails = TailUtil.getTailsArry(comptails);
 		int count = TailUtil.getCountSameTail(tails,comptails);
 		System.out.println();*/
 		for (int i = 2003; i < 2021; i++) {
-			String period =  i + "002";
+			String period =  i + "003";
 			String tails = periodtailsmap.get(period);
-			String pretails = periodtailsmap.get((i-1) + "002");
+			String pretails = periodtailsmap.get((i-1) + "003");
 			int same = 0;
 			if(StringUtils.isNotEmpty(tails) && StringUtils.isNotEmpty(pretails)){
 				String[] tailsarr = tails.split(",");
@@ -26,7 +28,7 @@ public class Test1 {
 				same = TailUtil.getCountSameTail(tailsarr,comptailsarr);
 
 			}
-			System.out.println(period+":"+tails+"--------" +same );
+			System.out.println(period+":"+historymap.get(period)+"@"+tails+"--------" +same );
 		}
 	}
 }
